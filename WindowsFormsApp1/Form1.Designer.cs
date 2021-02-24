1f3b617fc1102df6c0f1fc75190b11dd25dbd832
Michael Pagaran <mmpagaran64@gmail.com>
2021-02-24 13:18:31 +0800

    removed borders, mod right panel to gray backcolor

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace WindowsFormsApp1 {
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button2);
@@ -47,6 +48,7 @@ namespace WindowsFormsApp1 {
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(192)))), ((int)(((byte)(15)))));
+            this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Location = new System.Drawing.Point(27, 232);
             this.button4.Name = "button4";
@@ -58,6 +60,7 @@ namespace WindowsFormsApp1 {
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(17)))), ((int)(((byte)(19)))));
+            this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Location = new System.Drawing.Point(27, 173);
             this.button3.Name = "button3";
@@ -69,6 +72,7 @@ namespace WindowsFormsApp1 {
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(120)))), ((int)(((byte)(48)))));
+            this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Location = new System.Drawing.Point(27, 114);
             this.button2.Name = "button2";
@@ -80,7 +84,9 @@ namespace WindowsFormsApp1 {
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(58)))), ((int)(((byte)(108)))));
+            this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(27, 55);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(140, 53);
